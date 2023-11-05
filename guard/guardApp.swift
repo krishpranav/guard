a6f1db9c40a39858755362a042d755b39d9266f9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-05 13:33:53 +0530

    Initial Commit

---
+++ guard/guardApp.swift
@@ -0,0 +1,17 @@
+//
+//  guardApp.swift
+//  guard
+//
+//  Created by Krisna Pranav on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct guardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
