9e85e958948c670f0a7f15fef982cce3901a5e7b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-05 18:46:17 +0530

    guards: modules[SIP]

--- guard/modules/SIP.swift
+++ guard/modules/SIP.swift
@@ -23,10 +23,23 @@ class SIPCheck: Vulnerability {
             
             let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
             let output = String(data: outputData, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            
+            if output.lowercased().contains("enabled.") {
+                status = "SIP is enabled"
+                checkStatus = "Green"
+            } else {
+                status = "SIP is not enabled"
+                checkStatus = "Red"
+            }
+            
         } catch let e {
             print("Error checking \(name): \(e)")
             self.error = e
             checkStatus = "Yellow"
         }
+    }
+    
+    init() {
+        super.init(name: "Check system integerity protection (SIP)", description: "checks and verifies if SIP protection is enabled  or not", category: "Security", remediation: "To enable SIP restart your pc in recovery mode and run `csrutil enable`", severity: "High", documentation: "For more information please visist https://support.apple.com/en-us/HT204899", mitigation: "SIP is a very important security feature and you should keep it enabled to avoid unauthorized write access", docID: 20)
     }
 }
