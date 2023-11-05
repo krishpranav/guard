fc2e658d4a74fb074fbc00e57251c25e20358eb8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-05 13:42:56 +0530

    guard: core[vulnerability]

--- guard/core/vuln.swift
+++ guard/core/vuln.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import AppKit
 
 class Vulnerability: Identifiable {
     let id = UUID()
@@ -14,19 +15,28 @@ class Vulnerability: Identifiable {
     let category: String
     let remediation: String
     let severity: String
+    let documentation: String?
     var status: String?
+    let mitigation: String
     var error: Error?
+    var checkstatus: String?
+    let docID: Int32
     
-    init(name: String, description: String, category: String, remediation: String, severity: String, status: String) {
+    
+    init(name: String, description: String, category: String, remediation: String, severity: String, documentation: String, mitigation: String, checkstatus: String? = nil, docID: Int32) {
+        
         self.name = name
         self.description = description
         self.category = category
         self.remediation = remediation
         self.severity = severity
-        self.status = status
+        self.documentation = documentation
+        self.mitigation = mitigation
+        self.checkstatus = checkstatus
+        self.docID = docID
     }
     
     func check() {
-        fatalError("method is not implemeneted")
+        fatalError("The method should be implemented in subclasses.")
     }
 }
