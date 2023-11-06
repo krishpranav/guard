9a1e7e4cabc85a0f6d5b925c378d5c8b28cef751
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-06 17:14:31 +0530

    guard: modules[nfscheck]

--- guard/modules/nfscheck.swift
+++ guard/modules/nfscheck.swift
@@ -9,7 +9,47 @@ import Foundation
 
 class NfsCheck: Vulnerability {
     override func check() {
-            /// TODO
+        let taskone = Process()
+        let tasktwo = Process()
+        
+        /// PIPES
+        let pipeBetweenTasks = Pipe()
+        let outputPipe = Pipe()
+        
+        taskone.executableURL = URL(fileURLWithPath: "/bin/launchctl")
+        taskone.arguments = ["list"]
+        
+        tasktwo.executableURL = URL(fileURLWithPath: "/usr/bin/grep")
+        tasktwo.arguments = ["-c", "com.apple.nfsd"]
+        
+        taskone.standardOutput = pipeBetweenTasks
+        tasktwo.standardInput = pipeBetweenTasks
+        tasktwo.standardError = outputPipe
+        
+        do {
+            try taskone.run()
+            try tasktwo.run()
+                
+            taskone.waitUntilExit()
+            tasktwo.waitUntilExit()
+            
+            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            let output = String(data: outputData, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+            
+            if output == "0" {
+                status = "NFS Server is disabled"
+                checkStatus = "Green"
+            } else {
+                status = "NFS server is enabled"
+                checkStatus = "Red"
+            }
+            
+        } catch let e {
+            print("Error checking \(name): \(e)")
+            self.error = e
+            checkStatus = "Yellow"
+            status = "Error checking NFS server status"
+        }
     }
     
     init() {
